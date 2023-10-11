a6f6ee8886f2e2d912f41618bbc91af7aab04d9d
Aditi Umesh <43947310+AditiUmesh@users.noreply.github.com>
2023-10-11 06:54:14 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const accountName = "Aditi";
+let accountEmail = "aditiumesh@google.com";
+var accountPassword = "12345";
+accountCity = "Bengaluru"
+let accountState;
+
+// accountName="Namaste"
+accountEmail="aditi@gmail.com";
+accountPassword ="34567";
+accountCity = "Mysuru";
+
+console.log(accountName);
+console.table([accountName,accountEmail,accountPassword,accountCity,accountState])
